a89cf60e055d831c4cc9f968ed075fa6b8a43822
chamikaCN <chamikanandasiri97@gmail.com>
2022-12-16 15:52:16 +0530

    Initial Commit

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+require("dotenv").config();
+
+const sendMessage = require("./routes/sendMessage");
+
+const app = express();
+app.use(express.json());
+
+app.listen(process.env.PORT, () => {
+  console.log("Listening on PORT ", process.env.PORT);
+});
+
+app.use("/api/message", sendMessage);
+
+app.get("/", (req, res) => {
+  res.json({ message: "HELLLLOO" });
+});
