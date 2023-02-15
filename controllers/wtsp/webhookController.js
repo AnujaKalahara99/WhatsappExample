98ee2a8dcaf4072711d8ecc9d28df9f2f28b479b
chamikaCN <chamikanandasiri97@gmail.com>
2023-02-15 19:33:10 +0530

    test6

--- controllers/wtsp/webhookController.js
+++ controllers/wtsp/webhookController.js
@@ -17,22 +17,31 @@ const listenForReplies = async (req, res) => {
   createLog(JSON.stringify(body));
 
   if (body.object) {
-    if (
-      body.entry &&
-      body.entry[0].changes &&
-      body.entry[0].changes[0] &&
-      body.entry[0].changes[0].value.messages &&
-      body.entry[0].changes[0].value.messages[0]
-    ) {
-      let phone_number_id =
-        body.entry[0].changes[0].value.metadata.phone_number_id;
-      let from = body.entry[0].changes[0].value.messages[0].from; // extract the phone number from the webhook payload
-      let msg_body = body.entry[0].changes[0].value.messages[0].text.body; // extract the message text from the webhook payload
+    if (body.entry && body.entry[0].changes && body.entry[0].changes[0]) {
+      let waid, from, msg_body, status;
 
-      const message = await saveMessage(from, msg_body, true);
-      res.status(200).json(message);
-    } else {
-      res.sendStatus(404);
+      if (
+        body.entry[0].changes[0].statuses &&
+        body.entry[0].changes[0].statuses[0]
+      ) {
+        waid = body.entry[0].changes[0].statuses[0].id;
+        status = body.entry[0].changes[0].statuses[0].status;
+      }
+
+      if (
+        body.entry[0].changes[0].value.messages &&
+        body.entry[0].changes[0].value.messages[0]
+      ) {
+        phone_number_id =
+          body.entry[0].changes[0].value.metadata.phone_number_id;
+        from = body.entry[0].changes[0].value.messages[0].from; // extract the phone number from the webhook payload
+        msg_body = body.entry[0].changes[0].value.messages[0].text.body; // extract the message text from the webhook payload}
+
+        const message = await saveMessage(waid, from, msg_body, status, true);
+        res.status(200).json(message);
+      } else {
+        res.sendStatus(404);
+      }
     }
   }
 };
--- models/messageModel.js
+++ models/messageModel.js
@@ -3,31 +3,40 @@ const mongoose = require("mongoose");
 const messageSchema = mongoose.Schema(
   {
     index: { type: Number, required: true, unique: true },
+    waid: { type: String, required: true, unique: true },
     contact: { type: String, required: true },
     isRecieved: { type: Boolean, required: true },
-    text: { type: String, required: true },
-    isRead: { type: Boolean, required: true },
-    header: { type: Object, required: true },
+    status: { type: String, required: true },
+    header: {
+      type: { type: String },
+      data: { type: String },
+    },
     body: { type: String, required: true },
-    footer: { type: String, required: true },
+    footer: { type: String },
   },
   { timestamps: true }
 );
 
 const messageModel = mongoose.model("Messages", messageSchema);
 
-const saveMessage = async (number, msg, recieved) => {
+const saveMessage = async (waid, number, msg, status, recieved) => {
   const count = await messageModel.count();
+  const updated = await messageModel.findOneAndUpdate(
+    { waid },
+    { status },
+    { new: true }
+  );
+  if (updated) return updated;
   const messageSaved = await messageModel.create({
     index: count,
+    waid,
     contact: number,
     isRecieved: recieved,
-    isRead: false,
-    header: {},
+    status: status,
+    header: { type: "", data: "" },
     body: msg,
     footer: "",
   });
-
   return messageSaved;
 };
 
